d0b8a946a06bfa6e3a5a676b09b89339483a0fd0
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-07 22:15:58 -0400

    added config for mongodb credentials

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	userName: 'dallin',
+	pw: 'parker'
+}
--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+const {userName, pw} = require('./config');
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb://${userName}:${pw}@ds127994.mlab.com:27994/graphql_lyrical`;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
